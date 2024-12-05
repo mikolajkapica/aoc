31cd53212326f1fa3adfec5bd569f35c925f32b5
mkapica <mikolajkapica7@gmail.com>
2024-12-05 13:55:18 +0100

    use Nats instead of Ints, complete 1.1

--- Aoc/Day1/Solution.lean
+++ Aoc/Day1/Solution.lean
@@ -1,10 +1,10 @@
 def List.sequence {m α} [Monad m] (lst: List (m α)) : m (List α) := List.mapM id lst
 
-def parse (raw : String) : Option (List (Int × Int)) :=
+def parse (raw : String) : Option (List (Nat × Nat)) :=
   raw
     |>.splitOn "\n"
     |>.map (String.splitOn · "   ")
-    |>.map (List.map String.toInt?)
+    |>.map (List.map String.toNat?)
     |>.map List.sequence
     |>.filter Option.isSome
     |>.sequence
@@ -12,18 +12,27 @@ def parse (raw : String) : Option (List (Int × Int)) :=
     |>.map List.sequence
     |>.join
 
-def solve (input: List (Int × Int)) : Nat :=
+def rawInput := IO.FS.readFile "Aoc/Day1/input.txt"
+-- #eval parse <$> rawInput
+
+def fakeParsedInput: List (Nat × Nat) := [(3, 4), (4, 3), (2, 5), (1, 3), (3, 9), (3, 3)]
+
+def solve1 (input: List (Nat × Nat)) : Nat :=
   input
     |>.unzip
-    |> λ (a, b) => List.map (λ (x, y) => (x - y).natAbs) (a.mergeSort.zip b.mergeSort)
+    |> λ (a, b) => List.map (λ (x, y) => Int.natAbs (x - y)) (a.mergeSort.zip b.mergeSort)
     |>.sum
 
-def parseAndSolve := Option.map solve ∘ parse
+-- #eval solve1 fakeParsedInput
+-- #eval (Option.map solve1 ∘ parse) <$> rawInput
 
-def fakeRawInput: String := "3   4\n4   3\n2   5\n1   3\n3   9\n3   3"
-def fakeParsedInput: List (Int × Int) := [(3, 4), (4, 3), (2, 5), (1, 3), (3, 9), (3, 3)]
-def rawInput := IO.FS.readFile "Aoc/Day1/input.txt"
 
-#eval solve <$> parse fakeRawInput
-#eval solve fakeParsedInput
-#eval parseAndSolve <$> rawInput
+def solve2 (input : List (Nat × Nat)) : Nat :=
+  input
+    |>.unzip
+    |> λ (a, b) => a.map (λ x => x * List.count x b)
+    |>.sum
+
+
+#eval solve2 fakeParsedInput
+#eval (Option.map solve2 ∘ parse) <$> rawInput
